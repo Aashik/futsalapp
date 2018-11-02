4d8416de01223995be5130b293bdc4645ae2082d
ashik <theaaseek@gmail.com>
2018-11-01 22:48:21 -0700

    no change

--- src/main/java/com/futsalmanagement/futsalapp/service/GroundService.java
+++ src/main/java/com/futsalmanagement/futsalapp/service/GroundService.java
@@ -1,7 +1,12 @@
 package com.futsalmanagement.futsalapp.service;
 
+import com.futsalmanagement.futsalapp.entity.Futsal;
 import com.futsalmanagement.futsalapp.entity.Ground;
+
+import java.util.List;
 
 public interface GroundService  {
     Ground insert(Ground g);
+    List<Ground> getAll();
+    Ground getByFutsalId(String futsal_id);
 }
